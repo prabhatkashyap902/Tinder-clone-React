1bbda5408211c35a4f17e0f573f5b9c5bc41e8b1
prabhat <10p14ed0078@gmail.com>
2020-09-19 13:24:30 +0530

    icons added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/**HEader */}
+      <Header />
+      {/**Cards */}
+      {/**Footer */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../Css/Header.css"
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header_icon"/>
+            </IconButton>
+            <img className="tinder_img" src="https://cdn.worldvectorlogo.com/logos/tinder-2.svg"/>
+            <IconButton>
+                <ForumIcon></ForumIcon>
+            </IconButton>
+        </div>
+
+
+    )
+}
+
+export default Header
